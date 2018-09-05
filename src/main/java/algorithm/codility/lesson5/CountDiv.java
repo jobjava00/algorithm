2e7bfa99215691b9652f799fe9eb50d45af60c57
최창훈 <jobjava00@gmail.com>
2018-09-05 22:42:24 +0900

    [ADD] lesson5

---
+++ src/main/java/algorithm/codility/lesson5/CountDiv.java
@@ -0,0 +1,34 @@
+package algorithm.codility.lesson5;
+
+import org.junit.Assert;
+
+import static org.hamcrest.CoreMatchers.is;
+
+/**
+ * result : https://app.codility.com/demo/results/trainingVQ2BN7-NU9/?showingAll=1
+ */
+public class CountDiv {
+	public int solution(int A, int B, int K) {
+		int count = 0;
+		if (A % K == 0) count++;
+		if(A != B){
+			int firstDivided = A / K;
+			int secondDivided = B / K;
+			count += secondDivided - firstDivided;
+		}
+		return count;
+	}
+
+	public static void main(String[] args) {
+		CountDiv question = new CountDiv();
+		Assert.assertThat(question.solution(11, 13, 2), is(1));
+		Assert.assertThat(question.solution(10, 10, 7), is(0));
+		Assert.assertThat(question.solution(6, 11, 2), is(3));
+		Assert.assertThat(question.solution(11, 345, 17), is(20));
+		Assert.assertThat(question.solution(10, 10, 5), is(1));
+		Assert.assertThat(question.solution(0, 0, 11), is(1));
+		Assert.assertThat(question.solution(1, 1, 11), is(0));
+		Assert.assertThat(question.solution(11, 14, 2), is(2));
+		Assert.assertThat(question.solution(101, 123456789, 10000), is(12345));
+	}
+}
--- src/main/java/algorithm/codility/lesson5/PassingCars.java
+++ src/main/java/algorithm/codility/lesson5/PassingCars.java
@@ -13,7 +13,8 @@ import static org.hamcrest.CoreMatchers.is;
  * <p>
  * 0 represents a car traveling east,
  * 1 represents a car traveling west.
- * The goal is to count passing cars. We say that a pair of cars (P, Q), where 0 ≤ P < Q < N, is passing when P is traveling to the east and Q is traveling to the west.
+ * The goal is to count passing cars. We say that a pair of cars (P, Q), where 0 ≤ P < Q < N, is passing when P is traveling to the east and Q is traveling to
+ * the west.
  * <p>
  * For example, consider array A such that:
  * <p>
@@ -45,8 +46,8 @@ import static org.hamcrest.CoreMatchers.is;
  * <p>
  * N is an integer within the range [1..100,000];
  * each element of array A is an integer that can have one of the following values: 0, 1.
-
- ### 해석
+ * 
+ * ### 해석
  * 배열 A : N개의 정수로 이루어짐
  * A의 요소들은 도로상의 차들을 의미함.
  * - 0 : 동쪽으로 이동하는 차
@@ -55,8 +56,8 @@ import static org.hamcrest.CoreMatchers.is;
  * 0 ≤ P < Q < N
  * 1,000,000,000 초과하면 차의 쌍 수는 -1 리턴
  * 서로 지나가는 차의 쌍 수를 리턴하라!
-
- ### 풀이
+ * 
+ * ### 풀이
  * P보다 작은 값은 제외
  * (동쪽으로 이동하는 차) * (그 이후에 서쪽으로 이동하는 차)
  * ex) A[0] = 0, A[1] = 1, A[2] = 0, A[3] = 1, A[4] = 1
@@ -66,34 +67,34 @@ import static org.hamcrest.CoreMatchers.is;
  * A[3] 동쪽으로 이동 차량 * 서쪽으로 이동하는 차 = 2 * 1 = 2
  * A[4] 동쪽으로 이동 차량 * 서쪽으로 이동하는 차 = 2 * 1 = 2
  * 총합 5
-
- ### 제약사항
+ * 
+ * ### 제약사항
  * N의 범위는 정수 [1..100,000]
  * A의 각 요소의 값은 0, 1 중의 하나를 가진다.
  *
  * 결과 : https://app.codility.com/demo/results/trainingBJ5CRZ-U2C/?showingAll=1
  */
 public class PassingCars {
-    /**
-
-     */
-    public int solution(int[] A) {
+	/**
+	
+	 */
+	public int solution(int[] A) {
         int pCount = 0;
         int sum = 0;
         for (int i = 0; i < A.length; i++) {
             if (0 == A[i]) pCount++;
             else {
                 sum += pCount * A[i];
-                if(sum > 1000000000) return -1;
+                if (sum > 1000000000) return -1;
             }
         }
         return sum;
     }
 
-    public static void main(String[] args) {
-        PassingCars question = new PassingCars();
-        Assert.assertThat(question.solution(new int[]{0, 1, 0, 1, 1}), is(5));
-        Assert.assertThat(question.solution(new int[]{0, 0, 0, 1, 1}), is(6));
-        Assert.assertThat(question.solution(new int[]{1, 1, 0, 1, 1}), is(2));
-    }
+	public static void main(String[] args) {
+		PassingCars question = new PassingCars();
+		Assert.assertThat(question.solution(new int[] { 0, 1, 0, 1, 1 }), is(5));
+		Assert.assertThat(question.solution(new int[] { 0, 0, 0, 1, 1 }), is(6));
+		Assert.assertThat(question.solution(new int[] { 1, 1, 0, 1, 1 }), is(2));
+	}
 }
