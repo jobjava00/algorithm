a5444f60da85edba729c7c42f3457733eadface6
최창훈 <jobjava00@gmail.com>
2018-10-15 21:34:19 +0900

    [ADD] array, arrayList

---
+++ src/main/java/algorithm/hackerrank/practice/structures/ArrayListData.java
@@ -0,0 +1,41 @@
+package algorithm.hackerrank.practice.structures;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * ArrayList
+ * ArrayList를 배열에 담아서 이중 arrayList 형식으로 사용..
+ *  -> ArrayList의 개수를 알면 배열로 담는게 나은듯 하다.
+ * https://www.hackerrank.com/challenges/java-arraylist/problem
+ */
+public class ArrayListData {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int n = scanner.nextInt();
+		int d, q, x, y;
+
+		ArrayList[] arrayList = new ArrayList[n];
+
+		for (int i = 0; i < n; i++) {
+			d = scanner.nextInt();
+			arrayList[i] = new ArrayList();
+
+			for (int j = 0; j < d; j++) {
+				arrayList[i].add(scanner.nextInt());
+			}
+		}
+
+		q = scanner.nextInt();
+		for (int i = 0; i < q; i++) {
+			x = scanner.nextInt();
+			y = scanner.nextInt();
+			try {
+				System.out.println(arrayList[x - 1].get(y - 1));
+			} catch (IndexOutOfBoundsException e) {
+				System.out.println("ERROR!");
+			}
+		}
+	}
+}
--- src/main/java/algorithm/hackerrank/practice/structures/SubArray.java
+++ src/main/java/algorithm/hackerrank/practice/structures/SubArray.java
@@ -1,7 +1,37 @@
 package algorithm.hackerrank.practice.structures;
 
+import org.junit.Assert;
+
+import java.util.Scanner;
+
+import static org.hamcrest.CoreMatchers.is;
+
 /**
- * Created by uienw on 2018-10-14.
+ * 부분배열
+ * 연속적인 부분배열의 합이 음수인 배열의 조합 개수를 리턴하라!
+ * https://www.hackerrank.com/challenges/java-negative-subarray/problem
  */
 public class SubArray {
+	public int solution(int[] arr) {
+		int count = 0;
+
+		for (int i = 0; i < arr.length; i++) {
+			int sum = arr[i];
+			for (int j = i; j < arr.length; j++) {
+				if (i != j) {
+					sum += arr[j];
+				}
+				if (sum < 0) {
+					count++;
+				}
+			}
+		}
+		System.out.println(count);
+		return count;
+	}
+
+	public static void main(String[] args) {
+		SubArray question = new SubArray();
+		Assert.assertThat(question.solution(new int[] { 1, -2, 4, -5, 1 }), is(9));
+	}
 }
