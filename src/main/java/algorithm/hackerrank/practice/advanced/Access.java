d1ae209c3c78f29497a671f82cda73b1c7bb69b9
최창훈 <jobjava00@gmail.com>
2018-11-08 09:29:13 +0900

    [ADD] visitorPattern

--- src/main/java/algorithm/hackerrank/practice/advanced/Access.java
+++ src/main/java/algorithm/hackerrank/practice/advanced/Access.java
@@ -8,24 +8,24 @@ public class Access {
 	public static void main(String[] args) throws Exception {
 		DoNotTerminate.forbidExit();
 
-		try{
+		try {
 			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 			int num = Integer.parseInt(br.readLine().trim());
 			Object o;// Must be used to hold the reference of the instance of the class Solution.Inner.Private
 			o = new Inner().new Private();
 			((Inner.Private) o).powerof2(num);
 			System.out.println("An instance of class: " + o.getClass().getCanonicalName() + " has been created");
-
-		}//end of try
+		} //end of try
 
 		catch (DoNotTerminate.ExitTrappedException e) {
 			System.out.println("Unsuccessful Termination!!");
 		}
 	}//end of main
-	static class Inner{
-		private class Private{
-			private String powerof2(int num){
-				return ((num&num-1)==0)?"power of 2":"not a power of 2";
+
+	static class Inner {
+		private class Private {
+			private String powerof2(int num) {
+				return ((num & num - 1) == 0) ? "power of 2" : "not a power of 2";
 			}
 		}
 	}//end of Inner
